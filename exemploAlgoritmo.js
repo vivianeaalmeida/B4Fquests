5ddbdcbb2a598522b9c6a4cf63f1aae9c8a9507c
Viviane Andrade de Almeida <81475990+vivianeaalmeida@users.noreply.github.com>
2023-04-26 11:10:25 +0100

    Add files via upload

---
+++ exemploAlgoritmo.js
@@ -0,0 +1,6 @@
+Enquanto o vetor não está ordenado (while !ordenado)
+  Assume que o vetor está ordenado (ordenado = true)
+  Para cada elemento (for, arr[i])
+    Se o elemento e o próximo não estão na ordem certa
+      Troca elemento com o próximo (swap)
+      Marca vetor como não estando ordenado (ordenado = false)
