90c71725bb1a21efa7357bae120f12f967798481
Viviane Andrade de Almeida <81475990+vivianeaalmeida@users.noreply.github.com>
2023-04-27 09:53:10 +0100

    Update encontraPar.js

--- encontraPar.js
+++ encontraPar.js
@@ -1,5 +1,5 @@
 function encontraPar(array){
-    let primeiroPar = array[4];
+    let primeiroPar = array[0];
 
     for(let i = 0; i < array.length; i++){
         if(array[i] % 2 == 0 && array[i] != 0){
