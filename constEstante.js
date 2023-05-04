3e2f678fbf53b11a099238f74f9aed1c0a254d3c
Viviane Andrade de Almeida <81475990+vivianeaalmeida@users.noreply.github.com>
2023-05-04 09:39:55 +0100

    Create constEstante.js

---
+++ constEstante.js
@@ -0,0 +1,17 @@
+const ESTANTE = {
+    cor: "preto",
+    nrPrateleiras: 3,
+    prateleiras: [
+    {
+        numero: 1,
+        conteudo: ["livro", "microfone", "estojo"]
+    },
+    {
+        numero: 2,
+        conteudo: ["garrafa", "tosta", "garfo"]
+    },
+    {
+        numero: 3,
+        conteudo: []
+    }]
+}
