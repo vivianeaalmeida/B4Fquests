948509fded99fee4ad511522587717ca1188efb8
Viviane Andrade de Almeida <81475990+vivianeaalmeida@users.noreply.github.com>
2023-05-05 09:20:50 +0100

    Create examplesOfDateLongNumericEtc.js

---
+++ examplesOfDateLongNumericEtc.js
@@ -0,0 +1,4 @@
+let diaDaSemana = data.toLocaleString("pt-PT",{weekday:"long"}) // segunda-feira
+let diaDoAno = data.toLocaleString("pt-PT",{day:"numeric"}) // 2
+let mes = data.toLocaleString("pt-PT",{month:"long"}) // março
+let ano = data.toLocaleString("pt-PT",{year:"numeric"}) // 1999
