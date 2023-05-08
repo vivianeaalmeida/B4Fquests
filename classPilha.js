c7f06fb26aa334c85980aadf60f4e62d4cc27951
Viviane Andrade de Almeida <81475990+vivianeaalmeida@users.noreply.github.com>
2023-05-08 18:59:58 +0100

    Create classPilha.js

---
+++ classPilha.js
@@ -0,0 +1,41 @@
+class Pilha {
+    
+    capacidade;
+    lista;
+
+    constructor(capacidade){
+        if(capacidade != undefined){
+            this.capacidade = capacidade;
+        } else {
+            this.capacidade = Infinity;
+        }
+        this.lista = []
+    } //PASSO 1: OK
+    
+    //Não deve ser possível adicionar mais elementos à pilha do que a  da mesma.
+
+    adicionar(elem){
+        if(this.capacidade == this.lista.length){
+            return "Pilha cheia!"
+        }
+        else {
+            this.lista.push(elem)
+        }
+    }    
+
+    remover(){
+        if(this.lista.length == 0){  
+            return "Pilha vazia!"
+        } else {
+           this.lista.pop()
+        }
+    }
+
+    obterValor(){
+        if(this.lista.length == 0){
+            return "Pilha vazia!"
+        } else {
+            return this.lista[this.lista.length -1]
+        }
+    }
+}
